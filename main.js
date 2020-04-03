8ac31d5766ba26bf6ba077dcf24ef977b49a8025
Iwasson <Iwasson@users.noreply.github.com>
2020-04-02 23:39:00 -0700

    updates

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 const bot = require('node-rocketchat-bot');
 const keys = require('./keys.json');
+const fs = require('fs');
 
 //the bot will allow multiple games of 2 people to play chess
 //across rocket chat. It will require players to make an account
@@ -17,16 +18,16 @@ const keys = require('./keys.json');
 //h1 is a white space and all other alternate off of that
 //black starts on top and white starts on the bottom
 let defaultBoard = [
-    //a row
-    // 1  2  3  4  5  6  7  8
-      [R, N, B, Q, K, B, N, R], //h     R = rook
-      [P, P, P, P, P, P, P, P], //g     N = Knight
-      [                      ], //f     B = Bishop
-      [                      ], //e     Q = Queen
-      [                      ], //d     K = King
-      [                      ], //c     P = Pawn
-      [p, p, p, p, p, p, p, p], //b
-      [r, n, b, q, k, b, n, r]  //a
+  //a row
+  //1    2    3    4    5    6    7    8
+  ['R', 'N', 'B', 'Q', 'K', 'B', 'N', 'R'], //h     R = rook
+  ['P', 'P', 'P', 'P', 'P', 'P', 'P', 'P'], //g     N = Knight
+  [], //f     B = Bishop
+  [], //e     Q = Queen
+  [], //d     K = King
+  [], //c     P = Pawn
+  ['p', 'p', 'p', 'p', 'p', 'p', 'p', 'p'], //b
+  ['r', 'n', 'b', 'q', 'k', 'b', 'n', 'r']  //a
 ];
 
 
@@ -43,10 +44,99 @@ bot({
   onWake: async event => event.log.info(`${event.bot.username} ready`),
   // on message
   onMessage: async event => {
-    if (event.flags.isMentioned)
-      event.respond(`hi ${event.message.author.name} thanks for mentioning me`)
+    if (event.flags.isMentioned) {
+      const words = event.message.content.split(' ');
+      const operation = words[1] ? words[1].toLowerCase() : ''
+      event.log.info(`operation is "${operation}"`)
+      processCommand(event, words);
+    }
   }
 });
+
+//validates commands
+async function processCommand(event, words) {
+  switch (words[1].toLowerCase()) {
+    case "help":
+      event.respond("List of commands coming soon!");
+      break;
+    case "rules":
+      event.respond("So you want to know how to play chess huh?");
+      break;
+    case "move":
+      break;
+    case "make":
+      makeAccount(event);
+      break;
+    default:
+      event.respond("Incorrect Input, please try again or use help");
+      break;
+  }
+}
+
+//strips all punctuation and spaces from nicknames
+function simplifyNick(name) {
+  name = name.replace(/\s+/g, '');
+  name = name.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g, "");
+
+  return name;
+}
+
+//checks to see if a player has an account made or not
+async function hasAccount(user) {
+  var textByLine = fs.readFileSync("./database/accounts.txt").toString().split("\n");
+  let found = false;
+
+  console.log(user + "/////");
+
+  textByLine.forEach(account => {
+    fields = account.split("/");
+    console.log(fields[1] + " vs " + user);
+    if(fields[1] == user.toString()) {
+      found = true;
+    }
+  });
+
+  return found;
+}
+
+//makes an account for a player. This is done by taking their nick
+//off of rocket chat and creating a unique id for it.
+async function makeAccount(event) {
+  let user = event.message.author.name;
+  let userhash = parseInt(simplifyNick(user), 36);
+  let currentgameid = '';
+
+  //if the user already has an account then we need to abort and 
+  //give error
+  if (await hasAccount(user) == true) {
+    event.respond("Looks like you already have an account!");
+  }
+  else {
+    //each account is one line of text formatted as such
+    //uniqueid/rocketnick/currentgameid
+    //gameid will be null at first until they challenge someone
+
+    let account = userhash.toString() + "/" + user.toString() + "/" + currentgameid.toString() + "\n";
+
+    fs.appendFile("./database/accounts.txt", account, function (err) {
+      if (err) throw err;
+      console.log('Saved!');
+      event.respond("Your account has been made!");
+    });
+  }
+}
+
+
+//loads a player from the database
+async function loadPlayer(event) {
+
+}
+
+//saves a player to the database, also allows 
+//for updates on the player (keep track of stats).
+async function savePlayer(event, updates) {
+
+}
 
 
 //this function will save the game after each move
@@ -63,5 +153,10 @@ async function loadGame(event, game) {
 //takes in piece, position of piece and desitantion 
 //as arguments in words
 async function movePiece(words, event, game) {
+
+}
+
+//function that runs after each move to see if a player is in check
+async function checkMate(event, game) {
 
 }
